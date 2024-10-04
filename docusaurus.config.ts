d54e5a7811249c24c4599b03c30b28abc18a059c
João Ribeiro Bezerra <joaorb64@gmail.com>
2024-10-04 15:44:01 +0100

    Fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://https://joaorb64.github.io',
+  url: 'https://joaorb64.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',

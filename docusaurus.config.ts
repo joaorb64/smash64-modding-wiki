07ded2b21cb8673fb3c58b4fdc50931e66ae3a5a
João Ribeiro Bezerra <joaorb64@gmail.com>
2024-10-04 15:51:20 +0100

    Fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://joaorb64.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/smash64-modding-wiki/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
